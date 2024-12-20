5510a28fdaa9047fd49b24dd2a894267162d2835
sodiq muritala <Muritalasodiqbab1@gmail.com>
2024-12-20 17:04:17 +0100

    new Dtos added

--- Dtos/CartItemDto.cs
+++ Dtos/CartItemDto.cs
@@ -2,5 +2,12 @@
 {
     public class CartItemDto
     {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => price * Quantity;
     }
 }
--- Dtos/OrderDto.cs
+++ Dtos/OrderDto.cs
@@ -2,5 +2,15 @@
 {
     public class OrderDto
     {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public IEnumerable<CartItemDto> items { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public String PaymentMethod { get; set; }
+
     }
 }
