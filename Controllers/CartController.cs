9cf178f3ea38c5ed60391d79cf46e9684fefaad5
sodiq muritala <Muritalasodiqbab1@gmail.com>
2024-12-25 17:27:36 +0100

    logic on cart seervices added

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,7 +1,23 @@
-﻿namespace Book.Api.Controllers
-{
-    public class CartController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
 
+namespace Book.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly IcartServices _cartServices;
+        public CartController(IcartServices cartServices ) 
+        {
+            _cartServices = cartServices;
+        }
+        [HttpPost("{cartId}/items")]
+        public async Task <IActionResult> AddToCart(int cartId, [FromBody] AddToCartDto addToCartDto)
+        {
+            if (addToCartDto == null)
+                return BadRequest("Invalid cart item data.");
+            await _cartServices.AddToCartAsync(cartId, addToCartDto.BookId, addToCartDto.Quantity);
+            return Ok("Item added to cart successfully.");
+        }
     }
 }
